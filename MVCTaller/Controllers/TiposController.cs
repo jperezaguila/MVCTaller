1744e4a362c59ec4d91e30b252304f5621ccba68
PRUEBA2 <jperezaguila@yaho.es>
2015-11-18 14:28:37 +0100

    18 Nov

---
+++ MVCTaller/Controllers/TiposController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCTaller.Models;
+
+namespace MVCTaller.Controllers
+{
+    public class TiposController : Controller
+    {
+        private taller02Entities db = new taller02Entities();
+        // GET: Tipos
+        public ActionResult Index()
+        {
+            var data = db.Tipo;
+            return View(data);
+        }
+
+        public ActionResult Detalle(string nombre)
+        {
+            var nom = nombre.Replace("_", " ");
+            var data = db.Tipo.FirstOrDefault(o => o.nombre == nom);
+            if (data == null)
+
+                return RedirectToAction("Index");
+            return View(data);
+        }
+
+        public ActionResult Alta()
+        {
+            return View(new Tipo());
+        }
+
+        [HttpPost]
+        public ActionResult Alta(Tipo model)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tipo.Add(model);
+                db.SaveChanges();
+                return View(new Tipo());
+            }
+            return View(model);
+
+        }
+    }
+
+}
---
+++ MVCTaller/Controllers/VehiculosController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCTaller.Models;
+
+namespace MVCTaller.Controllers
+{
+    public class VehiculosController : Controller
+    {
+        taller02Entities db = new taller02Entities();
+        
+        // GET: Vehiculos
+        public ActionResult Index()
+        {
+            return View(db.Vehiculos);
+
+        }
+
+        [HttpPost]
+        public ActionResult Alta(Vehiculos model)
+        {
+            db.Vehiculos.Add(model);
+            db.SaveChanges();
+            return Json(model);
+        }
+        
+
+    }
+}
