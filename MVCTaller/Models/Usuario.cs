d3345312925586ced157b55f209445988343ff9d
jperezaguila <jperezaguila@yahoo.es>
2016-05-31 16:37:50 +0200

    Add files via upload

---
+++ MVCTaller/Models/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCTaller.Models
+{
+	
+        public partial class Usuario
+        {
+            public int id { get; set; }
+            public string nombre { get; set; }
+            public string apellidos { get; set; }
+            public string login { get; set; }
+            public string email { get; set; }
+            public string password { get; set; }
+            public int rolId { get; set; }
+
+            public virtual Rol Rol { get; set; }
+        }
+    }
